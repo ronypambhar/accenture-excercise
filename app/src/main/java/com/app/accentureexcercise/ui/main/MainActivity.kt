677d3376cbcb50138e5b7d7da03ab51ae67d87a5
ronypambhar <ronypambhar@gmail.com>
2019-06-16 22:53:20 +0530

    Commited By : Rohan Pambhar

--- app/src/main/java/com/app/accentureexcercise/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/accentureexcercise/ui/main/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+
     }
 
     private fun init() {
