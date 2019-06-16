cafa5b2cb62d8a430529e0fdd6644bdea75a80dc
ronypambhar <ronypambhar@gmail.com>
2019-06-16 16:45:01 +0530

    Commited By : Rohan Pambhar

--- app/src/main/java/com/app/accentureexcercise/MainActivity.kt
+++ app/src/main/java/com/app/accentureexcercise/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.app.accentureexcercise
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +6,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
